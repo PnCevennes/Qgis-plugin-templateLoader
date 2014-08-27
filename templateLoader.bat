6123e9b1ebd08914e7353bcb82ce2376a87f882f
amandine-sahl <amandine.sahl@gmail.com>
2014-08-27 19:10:55 +0200

    update windows installer add reg entry

--- templateLoader.bat
+++ templateLoader.bat
@@ -1,3 +1,15 @@
 @echo off
-IF exist %UserProfile%\.qgis2\python\plugins ( echo QGIS dir exists )
-ELSE ( echo QGIS dir doesn't exists contact an administrator Exit)
+If not exist %UserProfile%\.qgis2\python\plugins (
+	Echo QGIS dir doesn't exists contact an administrator 
+	Exit /b
+) ELSE Echo QGIS dir exist
+
+IF exist %UserProfile%\.qgis2\python\plugins\templateLoader (
+	rmdir /S /Q %UserProfile%\.qgis2\python\plugins\templateLoader
+	Echo QGIS templateLoader dir deleted
+)
+
+mkdir %UserProfile%\.qgis2\python\plugins\templateLoader
+xcopy /D /E /C /R /H /I /K /Y /Q "templateLoader" %UserProfile%\.qgis2\python\plugins\templateLoader
+
+REG ADD "HKEY_CURRENT_USER\Software\QGIS\QGIS2\PythonPlugins" /v "templateLoader" /d "true"  /f
